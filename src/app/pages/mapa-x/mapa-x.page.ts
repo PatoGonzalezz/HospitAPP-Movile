839ee27cbaab18635cd5dfa648d99358fe3143df
PatoGonzalezz <pato12lc@gmail.com>
2023-10-16 18:38:46 -0300

    Se implementa MapBox

--- src/app/pages/mapa-x/mapa-x.page.ts
+++ src/app/pages/mapa-x/mapa-x.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mapa-x',
   templateUrl: './mapa-x.page.html',
   styleUrls: ['./mapa-x.page.scss'],
 })
+
+
 export class MapaXPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
   }
 
+  ngOnInit() {
+    const map = new mapboxgl.Map({
+      container: 'map-container', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-71.533143,-33.033704], // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      });
+
+      
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoicGF0MWhubyIsImEiOiJjbG5naWgyOXIwYnF2Mm1vOXhueWFtbXM4In0.zfLtRF__vmCcRycCOuJbPw';
+
 if (environment.production) {
   enableProdMode();
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+
+// if (navigator.geolocation){
+//   alert('Navegador no soporta la Geolocation');
+//   throw new Error ('Navegador no soporta la Geolocation');
+// }
