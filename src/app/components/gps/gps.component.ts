d56362da133aab261e611ae47e25730dfbad0c47
PatoGonzalezz <pato12lc@gmail.com>
2023-11-21 17:09:44 -0300

    Se activa la ubicación

--- src/app/components/gps/gps.component.ts
+++ src/app/components/gps/gps.component.ts
@@ -17,6 +17,18 @@ export class GpsComponent  implements OnInit {
       center: [-71.533143,-33.033704], // starting position [lng, lat]
       zoom: 14, // starting zoom
       });
+      map.addControl(
+        new mapboxgl.GeolocateControl({
+        positionOptions: {
+        enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+        })
+        );
+        
   }
 
 }
