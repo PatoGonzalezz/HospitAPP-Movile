383c400f839617ad7c0ea7556fbecb71c86c9eb2
PatoGonzalezz <pato12lc@gmail.com>
2023-11-23 20:23:07 -0300

    Se agregan zoom y orientación

--- src/app/components/gps/gps.component.ts
+++ src/app/components/gps/gps.component.ts
@@ -1,34 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import mapboxgl from 'mapbox-gl';
 
+
 @Component({
   selector: 'app-gps',
   templateUrl: './gps.component.html',
   styleUrls: ['./gps.component.scss'],
 })
-export class GpsComponent  implements OnInit {
+export class GpsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
     const map = new mapboxgl.Map({
       container: 'map-container', // container ID
-      style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-71.533143,-33.033704], // starting position [lng, lat]
+      style: 'mapbox://styles/pat1hno/clpa54zns003501p6008q57rj', // style URL
+      center: [-71.533143, -33.033704], // starting position [lng, lat]
       zoom: 14, // starting zoom
-      });
-      map.addControl(
-        new mapboxgl.GeolocateControl({
+    });
+    map.addControl(
+      new mapboxgl.GeolocateControl({
         positionOptions: {
-        enableHighAccuracy: true
+          enableHighAccuracy: true
         },
-        // When active the map will receive updates to the device's location as it changes.
+        // Para sacar la ubicación del teléfono.
         trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        // Orientación del teléfono (true activada.)
         showUserHeading: true
-        })
-        );
-        
+      })
+    );
+    //Zoom y control de orientación
+    map.addControl(new mapboxgl.NavigationControl());
+
+    
+      
   }
 
 }
