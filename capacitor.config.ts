43ad62ffe34956865822e480906a44133b22650b
PatoGonzalezz <pato12lc@gmail.com>
2023-11-26 22:41:08 -0300

    Se termina app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.codingtechnyks.gps',
-  appName: 'SkeletonApp',
+  appName: 'HospitAPP',
   webDir: 'www',
   server: {
     androidScheme: 'https'
